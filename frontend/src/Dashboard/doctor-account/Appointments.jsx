237942fb18c93fc1561e0860c79c550db7845bab
ayush patidar <ayushpatidar7441@gmail.com>
2024-07-22 12:35:13 +0530

    Ready for delpoyment

--- frontend/src/Dashboard/doctor-account/Appointments.jsx
+++ frontend/src/Dashboard/doctor-account/Appointments.jsx
@@ -19,14 +19,14 @@ const Appointments = ({ appointments }) => {
       {appointments?.map(item => (
         <tr key={item._id}>
           <td scope='row' className='flex items-center px-6 py-4 text-gray-900 whitespace-nowrap'>
-            <img src={item.user.photo} className='w-8 h-8 rounded-full ' alt='' />
+            <img src={item?.user?.photo} className='w-8 h-8 rounded-full ' alt='' />
             <div className='pl-3'>
-              <p className='text-base font-semibold'>{item.user.name}</p>
-              <p className='text-nomal text-gray-500' >{item.user.email}</p>
+              <p className='text-base font-semibold'>{item?.user?.name}</p>
+              <p className='text-nomal text-gray-500' >{item?.user?.email}</p>
             </div>
           </td>
 
-          <td className='px-6 py-4'>{item.user.gender}</td>
+          <td className='px-6 py-4'>{item?.user?.gender}</td>
           <td className='px-6 py-4'>
             {item.isPaid && (<div className='flex items-center '><div className='h-2.5 w-2.5 rounded-full bg-green-500 mr-2'></div>
               Paid
@@ -36,8 +36,8 @@ const Appointments = ({ appointments }) => {
               UnPaid
             </div>)}
           </td>
-          <td className='px-6 py-4'>{item.ticketPrice}</td>
-          <td className='px-6 py-4'>{formateDate(item.createdAt)}</td>
+          <td className='px-6 py-4'>{item?.ticketPrice}</td>
+          <td className='px-6 py-4'>{formateDate(item?.createdAt)}</td>
         </tr>
       ))}
     </tbody>
